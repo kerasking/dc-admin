cdf2c6b2fdcfcb080bbb4d092b71858efde650dd
lzq2016 <357881371@qq.com>
2017-12-21 00:52:16 +0800

    lzq

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,15 @@ Page({
   data: {
       pageHeight: app.globalData.window.height,
       pageWidth: app.globalData.window.width,
-      listWidth: (app.globalData.window.width*2 - 1)/2
+      listWidth: (app.globalData.window.width*2 - 1)/2,
+      currentType:0
   },
   onLoad: function () {
       console.log(app.globalData.window);
-  }
-  
+  },
+  refreshList: function (event) {
+    this.setData({
+      currentType: parseInt(event.currentTarget.dataset.type)
+    });
+  },
 })
